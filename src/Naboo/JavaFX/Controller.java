fad398d5d666ab8325b688c353a537a039a7c76a
pojessi <pojei@hotmail.it>
2022-11-29 17:24:15 +0100

    aggiunta librerie

--- src/Naboo/JavaFX/Controller.java
+++ src/Naboo/JavaFX/Controller.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 //import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 
 public class Controller {
 
